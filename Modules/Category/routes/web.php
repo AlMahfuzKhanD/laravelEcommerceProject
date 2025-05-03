81539236dd597954c23d07de783a6c2783122a7e
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-05-03 11:08:13 +0600

    category module created

--- Modules/Category/routes/web.php
+++ Modules/Category/routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Category\Http\Controllers\CategoryController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('category', CategoryController::class)->names('category');
+Route::middleware(['web', 'auth'])->prefix('admin/categories')->name('admin.categories.')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('index');
+    Route::get('/create', [CategoryController::class, 'create'])->name('create');
+    Route::post('/', [CategoryController::class, 'store'])->name('store');
+    Route::get('/{id}/edit', [CategoryController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('destroy');
 });
